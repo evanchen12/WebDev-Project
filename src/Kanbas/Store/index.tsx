b948d25ec35788963001334813fb172659f9f128
evanchen12 <71461932+evanchen12@users.noreply.github.com>
2024-03-24 14:04:38 -0400

    quiz, tfq reducer

--- src/Kanbas/Store/index.tsx
+++ src/Kanbas/Store/index.tsx
@@ -1,15 +1,27 @@
 import { configureStore } from "@reduxjs/toolkit";
 import modulesReducer from "../Courses/Modules/modulesReducer";
+import quizzesReducer from "../Courses/Quiz/quizzesReducer";
+import tfQReducer from "../Courses/Quiz/tfQReducer";
 
 export interface KanbasState {
   modulesReducer: {
     modules: any[];
     module: any;
   };
+  quizzesReducer: {
+    quizzes: any[];
+    quiz: any;
+  };
+  tfQReducer: {
+    tfQ: any[];
+    tf: any;
+  }
 }
 const store = configureStore({
   reducer: {
-    modulesReducer
+    modulesReducer,
+    quizzesReducer,
+    tfQReducer
   }
 });
 
